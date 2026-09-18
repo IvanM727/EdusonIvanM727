2d715b5a5ff5fb44df66f7027b0c1b0b857cdc75
IvanM727 <meshkov101@gmail.com>
2026-09-18 22:18:25 +0500

    Initial commit by Eduson

---
+++ Eduson.py
@@ -0,0 +1,17 @@
+# TODO add new phrase
+print("Numbers comarison")
+print("Loading...")
+b = int(input("Enter b = "))
+c = int(input("Enter c = "))
+print("b =", b, ",", "c =", c)
+if (b >= c):
+    a = True
+else:
+    a = False
+    """оно не работает почему-то, если b=3 - но почему - вопрос.
+    """
+print("Is b = c or b > c?")
+if a == True:
+    print("Yes")
+else:
+    print("No")
